3cc358fb98de433ec53e929c74181044df023ff6
creagent31-cmyk <creagent31@gmail.com>
2026-07-23 14:55:17 +0200

    Create email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,47 @@
+import smtplib
+
+from email.mime.text import MIMEText
+
+from config import (
+    EMAIL_USER,
+    EMAIL_PASSWORD,
+    EMAIL_TO
+)
+
+
+
+def send(html):
+
+
+    msg=MIMEText(
+        html,
+        "html"
+    )
+
+
+    msg["Subject"]="📰 Cresco Media Monitoring"
+
+    msg["From"]=EMAIL_USER
+
+    msg["To"]=",".join(
+        EMAIL_TO
+    )
+
+
+    with smtplib.SMTP_SSL(
+        "smtp.gmail.com",
+        465
+    ) as server:
+
+
+        server.login(
+            EMAIL_USER,
+            EMAIL_PASSWORD
+        )
+
+
+        server.sendmail(
+            EMAIL_USER,
+            EMAIL_TO,
+            msg.as_string()
+        )
